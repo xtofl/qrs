800fa07801b9aa9afd75759c45b54f5c0d1263c9
xtofl <kristoffel.pirard@gmail.com>
2020-01-26 09:38:55 +0100

    Use secret in message

--- qrs.sh
+++ qrs.sh
@@ -8,10 +8,13 @@ function generate_qr() {
 	while read id; do
 		L_OUTPUT=${OUTPUT-$PREFIX$id.$SUFFIX}
 		echo writing to $L_OUTPUT
+		CONTENT=$MESSAGE-$id
+		THIS_SECRET=$(echo $SECRET-$CONTENT | md5sum | cut -c 1-10)
+		echo $THIS_SECRET
 		qrencode \
 			--output $L_OUTPUT \
 		       	--type $TYPE \
-			"$MESSAGE-$id"
+			"$CONTENT-$THIS_SECRET"
 	done
 }
 
